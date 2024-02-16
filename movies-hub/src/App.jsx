4f2d37eb7472592c33921c298830f20237fccbd9
Akshay Jain <akshayjain2823@gmail.com>
2024-02-16 13:08:31 +0530

    Basic redux setting & testing done

--- movies-hub/src/App.jsx
+++ movies-hub/src/App.jsx
@@ -1,17 +1,22 @@
 import { useState, useEffect } from "react";
 import { fetchDataFromApi } from "./utils/api";
+import { useDispatch, useSelector } from "react-redux";
+import { getApiConfiguration } from "./store/homeSlice.js";
 
 function App() {
+	const dispatch = useDispatch();
 	const [count, setCount] = useState(0);
 	useEffect(() => {
 		apiTesting();
 	}, []);
 	const apiTesting = () => {
 		fetchDataFromApi("/movie/popular").then((res) => {
+			dispatch(getApiConfiguration(res));
 			console.log(res);
 		});
 	};
-	return <div className='App'>App</div>;
+	const { url } = useSelector((state) => state.home);
+	return <div className='App'>{url?.total_pages}</div>;
 }
 
 export default App;
