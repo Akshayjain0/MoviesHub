059c271d34624fd06a464ed0c47bad07ed709abd
Akshay Jain <akshayjain2823@gmail.com>
2024-02-17 23:24:16 +0530

    Page & Component structure done

--- movies-hub/src/App.jsx
+++ movies-hub/src/App.jsx
@@ -2,7 +2,8 @@ import { useState, useEffect } from "react";
 import { fetchDataFromApi } from "./utils/api";
 import { useDispatch, useSelector } from "react-redux";
 import { getApiConfiguration } from "./store/homeSlice.js";
-
+import { PageNotFound, Home, Details, SearchResult } from "./pages/index.js";
+import { Header, Footer } from "./components/index.js";
 function App() {
 	const dispatch = useDispatch();
 	const [count, setCount] = useState(0);
